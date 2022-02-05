53868e470511fe7619bc73a2067228089dbb8695
Noah Lepage <noahlepage00@gmail.com>
2022-02-05 15:50:59 -0500

    commit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,10 +14,10 @@ public class PlayerController : MonoBehaviour
     public GameObject CenterEyeCamera;
     public GameObject ForwardDirection;
     public float MinRunSpeed = 0.8f;
-    public float JumpSpeed = 20f;
-
+    public float JumpSpeed = 0.5f;
     public float Speed = 70f;
-    private float HandSpeed;
+
+    private float Gravity = 9.81f;
 
     private float[] VelocityArray;
 
@@ -25,8 +25,13 @@ public class PlayerController : MonoBehaviour
     private ControllerVelocity LeftController;
     private ControllerVelocity RightController;
     private Vector3 PlayerVelocity;
+    private bool isJumping = false;
 
     private CharacterController playerController;
+
+    public float AverageHandSpeed {
+        get => VelocityArray.Average();
+    }
 
     private void Awake() {
         LeftController = LeftHandObj.GetComponent<ControllerVelocity>();
@@ -35,25 +40,31 @@ public class PlayerController : MonoBehaviour
     }
 
     private void Start() {
-        VelocityArray = new float[20];
+        VelocityArray = new float[30];
     }
 
-    private void FixedUpdate() {
-        FrameCounter++;
-        if (FrameCounter >= VelocityArray.Length - 1) {
-            FrameCounter = 0;
-        }
-
+    private void Update() {
+        if (Time.timeSinceLevelLoad < 1f) return;
+        
+        TrackHandSpeed();
         SetForwardDirection();
-        HandSpeed = GetAverageHandSpeed();
 
         if (playerController.isGrounded) {
             ManageRun();
             ManageJump();
         }
 
-        PlayerVelocity.y += -9.81f * Time.deltaTime;
-        playerController.Move(PlayerVelocity * Time.deltaTime);
+        // Applying gravity to player
+        // PlayerVelocity.y -= Gravity * Time.deltaTime;
+        // playerController.Move(PlayerVelocity * Time.deltaTime);
+    }
+
+    private void TrackHandSpeed() {
+        FrameCounter++;
+        if (FrameCounter >= VelocityArray.Length - 1) {
+            FrameCounter = 0;
+        }
+        VelocityArray[FrameCounter] = (LeftController.Velocity + RightController.Velocity) / 2;
     }
 
     private void SetForwardDirection() {
@@ -62,21 +73,38 @@ public class PlayerController : MonoBehaviour
     }
 
     private void ManageRun() {
-        if (playerController.velocity.magnitude > 1f) {
-            playerController.Move(ForwardDirection.transform.forward * HandSpeed * Speed * Time.deltaTime);
+        if (IsMoving()) {
+            playerController.Move(ForwardDirection.transform.forward * AverageHandSpeed * Speed * Time.deltaTime);
         }
     }
 
     private void ManageJump() {
-        if (LeftController.Velocity < 1f || RightController.Velocity < 1f) return;
+        if (!CanJump()) return;
 
-        if (LeftHandObj.transform.position.y > CenterEyeCamera.transform.position.y && RightHandObj.transform.position.y > CenterEyeCamera.transform.position.y) {
-            PlayerVelocity.y += Mathf.Sqrt(1f * -3.0f * -9.81f);
+        isJumping = JumpAction();
+        if (isJumping) {
+            txt.text = "jump";
+            playerController.Move(Vector3.up * JumpSpeed * Gravity * Time.deltaTime);
+            // StartCoroutine(EnableJump());
+            // PlayerVelocity.y += Mathf.Sqrt(JumpSpeed * 3.0f * Gravity);
+        }
+        else {
+            txt.text = "not jumping";
         }
     }
 
-    private float GetAverageHandSpeed() {
-        VelocityArray[FrameCounter] = (LeftController.Velocity + RightController.Velocity) / 2;
-        return VelocityArray.Average();
+    IEnumerator EnableJump() {
+        Gravity = -Gravity;
+        yield return new WaitForSeconds(0.3f);
+        Gravity = -Gravity;
     }
+
+    // Checks if player has a speed greater than 1 (joystick movement);
+    private bool IsMoving() => playerController.velocity.magnitude > 1f;
+
+    // Can jump if both controllers are fast enough & player isn't already jumping
+    private bool CanJump() => LeftController.Velocity > 1f && RightController.Velocity > 1f;
+
+    // Checks if player does the jump action (raise both hands above head)
+    private bool JumpAction() => LeftHandObj.transform.position.y > CenterEyeCamera.transform.position.y && RightHandObj.transform.position.y > CenterEyeCamera.transform.position.y;
 }
