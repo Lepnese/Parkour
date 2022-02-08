fd671d6cf8702894599b01dda78e28cc1a1b69d7
Noah Lepage <noahlepage00@gmail.com>
2022-02-07 19:28:15 -0500

    commit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,65 +9,71 @@ public class PlayerController : MonoBehaviour
 {
     public Text txt;
 
+    #region Public Fields
     public GameObject _groundChecker;
     public GameObject LeftHandObj;
     public GameObject RightHandObj;
     public GameObject CenterEyeCamera;
-    public GameObject ForwardDirection;
-    public float MinRunSpeed = 0.8f;
-    public float Speed = 70f;
+    public float Speed = 2f;
     public float JumpHeight = 2f;
     public LayerMask Ground;
-    public float SphereSize;
+    #endregion
 
-    private float Gravity = 9.81f;
+    private CharacterController playerController;
+    
+    private int FrameCounter;
+    private const float Gravity = 9.81f;
     private bool _isGrounded;
 
     private float[] VelocityArray;
     private Vector3[] SpeedArray;
-
-    private int FrameCounter;
-    private int FrameCounter2;
+    
     private ControllerVelocity LeftController;
     private ControllerVelocity RightController;
+    
     private Vector3 PlayerVelocity;
-
     private Vector3 LastFramePosition;
+    private Vector3 ForwardDirection;
+    private Vector3 AveragePlayerSpeed;
 
-    private CharacterController playerController;
+    private Transform _transform;
+    private Transform CameraTransform;
+
 
     public float AverageHandSpeed {
         get => VelocityArray.Average();
-    }
-
-    public Vector3 AverageCurSpeed {
-        get => SpeedArray.Aggregate(Vector3.zero, (acc, v) => acc + v) / SpeedArray.Length;
     }
 
     private void Awake() {
         LeftController = LeftHandObj.GetComponent<ControllerVelocity>();
         RightController = RightHandObj.GetComponent<ControllerVelocity>();
         playerController = GetComponent<CharacterController>();
-        
     }
 
     private void Start() {
         VelocityArray = new float[60];
         SpeedArray = new Vector3[30];
-        LastFramePosition = transform.position;
+        _transform = transform;
+        CameraTransform = CenterEyeCamera.transform;
+        LastFramePosition = _transform.position;
     }
 
     private void Update() {
-        if (Time.timeSinceLevelLoad < 1f) return;        
+        if (Time.timeSinceLevelLoad < 1f) return;
+        FrameCounter++;
 
-        txt.text = AverageCurSpeed.magnitude.ToString();
+        AveragePlayerSpeed = GetAveragePlayerSpeed();
+        ForwardDirection = CameraTransform.TransformDirection(Vector3.forward);
+
+        txt.text = AveragePlayerSpeed.magnitude.ToString();
 
         TrackHandSpeed();
-        SetForwardDirection();
 
-        _isGrounded = Physics.CheckSphere(_groundChecker.transform.position, SphereSize, Ground, QueryTriggerInteraction.Ignore);
-        if (_isGrounded && PlayerVelocity.y < 0)
+        // _isGrounded = Physics.CheckSphere(_groundChecker.transform.position, SphereSize, Ground, QueryTriggerInteraction.Ignore);
+        _isGrounded = Physics.Raycast(_groundChecker.transform.position, transform.TransformDirection(Vector3.down), 0.05f, Ground);
+        if (_isGrounded && PlayerVelocity.y < 0.1f) {
             PlayerVelocity.y = 0f;
+        }
 
         if (_isGrounded) {
             ManageRun();
@@ -80,47 +86,37 @@ public class PlayerController : MonoBehaviour
         PlayerVelocity.y -= Gravity * Time.deltaTime;
         playerController.Move(PlayerVelocity * Time.deltaTime);
         
-        PlayerVelocity.x = AverageCurSpeed.x;
-        PlayerVelocity.z = AverageCurSpeed.z;
+        PlayerVelocity.x = AveragePlayerSpeed.x;
+        PlayerVelocity.z = AveragePlayerSpeed.z;
 
-        LastFramePosition = transform.position;
+        LastFramePosition = _transform.position;
     }
 
     private void TrackPlayerSpeed() {
-        FrameCounter2++;
-        if (FrameCounter2 >= SpeedArray.Length - 1) {
-            FrameCounter2 = 0;
-        }
-        SpeedArray[FrameCounter2] = (transform.position - LastFramePosition) / Time.smoothDeltaTime;
+        SpeedArray[FrameCounter % SpeedArray.Length] = (_transform.position - LastFramePosition) / Time.smoothDeltaTime;
     }
 
     private void TrackHandSpeed() {
-        FrameCounter++;
-        if (FrameCounter >= VelocityArray.Length - 1) {
-            FrameCounter = 0;
-        }
-        VelocityArray[FrameCounter] = (LeftController.Velocity + RightController.Velocity) / 2;
-    }
-
-    private void SetForwardDirection() {
-        float yRotation = CenterEyeCamera.transform.eulerAngles.y;  
-        ForwardDirection.transform.eulerAngles = new Vector3(0, yRotation, 0);
+        VelocityArray[FrameCounter % VelocityArray.Length] = (LeftController.Velocity + RightController.Velocity) / 2;
     }
 
     private void ManageRun() {
         if (IsMoving()) {
-            playerController.SimpleMove(ForwardDirection.transform.forward * AverageHandSpeed * Speed);
+            playerController.SimpleMove(ForwardDirection * AverageHandSpeed * Speed);
             // playerController.Move(ForwardDirection.transform.forward * AverageHandSpeed * Speed * Time.smoothDeltaTime);
         }
     }
 
     private void ManageJump() {        
         if (JumpAction()) {
-            PlayerVelocity.x = AverageCurSpeed.x;
-            PlayerVelocity.z = AverageCurSpeed.z;
+            PlayerVelocity.x = AveragePlayerSpeed.x;
+            PlayerVelocity.z = AveragePlayerSpeed.z;
             PlayerVelocity.y += Mathf.Sqrt(JumpHeight * 2f * Gravity);
         }
     }
+
+    // Returns the average velocity (from SpeedArray) of the player
+    private Vector3 GetAveragePlayerSpeed() => SpeedArray.Aggregate(Vector3.zero, (acc, v) => acc + v) / SpeedArray.Length;
 
     // Checks if player has a speed greater than 1 (joystick movement);
     private bool IsMoving() => playerController.velocity.magnitude > 1f;
