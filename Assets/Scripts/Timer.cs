30aef9f864564f3b8390b3c63da80517a4b9c8cd
Noah Lepage <noahlepage00@gmail.com>
2022-02-23 09:34:01 -0500

    commit

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,24 +1,36 @@
+using System;
 using System.Collections;
+using TMPro;
 using UnityEngine.UI;
 using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
-
-    public Text timerText;
+    private TextMeshProUGUI timerText;
     private float startTime;
-    void Start()
-    {
-        startTime = Time.time;
+    private bool timerStarted;
+
+    private void Awake() {
+        timerText = GetComponent<TextMeshProUGUI>();
     }
 
-    void Update()
+    void Start()
     {
-        float t = Time.time - startTime;
+        GameManager.GameStart += StartTimer;
+    }
+
+    private void StartTimer() {
+        timerStarted = true;
+    }
+    
+    void Update() {
+        if (!timerStarted) return;
+        
+        float t = Time.time - GameManager.startTime;
         int minutes = Mathf.FloorToInt(t / 60F);
         int seconds = Mathf.FloorToInt(t - minutes * 60);
-        int millilseconds = Mathf.FloorToInt(t * 1000) %1000;
-
-        timerText.text = string.Format("{00:00}:{1:00}:{2:000}", minutes, seconds, millilseconds);
+        int millilseconds = Mathf.FloorToInt(t * 100) % 100;
+        
+        timerText.text = string.Format("{0:}:{1:00}:{2:00}", minutes, seconds, millilseconds);
     }
 }
