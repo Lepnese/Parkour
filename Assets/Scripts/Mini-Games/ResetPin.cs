d6184e0752da92f9717dc0ebd343659fd84ea625
Charles Vignon <202033611@edu.clg.qc.ca>
2022-04-27 09:23:25 -0400

    Revert "ok"
    This reverts commit 532f3e5da75ad35e5902130e1428d62e2302ca8b.

--- Assets/Scripts/Mini-Games/ResetPin.cs
+++ Assets/Scripts/Mini-Games/ResetPin.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class ResetPin : MonoBehaviour
 {
     //[SerializeField] private VoidEvent OnPressedEvent;
-    //Pin pin;
-    //Vector3 PositionAtStart;
-    //void Start()
-    //{
-    //    pin = GetComponent<Pin>();
-    //    PositionAtStart = pin.transform.position;
-    //}
-    //void OnTriggerEnter(Collider other)
-    //{
-    //    if (other.CompareTag("Player"))
-    //    {
-    //        pin.transform.position = PositionAtStart;
-    //    }
-    //}
+    Pin pin;
+    Vector3 PositionAtStart;
+    void Start()
+    {
+        pin = GetComponent<Pin>();
+        PositionAtStart = pin.transform.position;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            pin.transform.position = PositionAtStart;
+        }
+    }
 }
